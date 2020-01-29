20644d73ed5100813b58d071e1c684e430e42e58
allison cortez <allizetorc@gmail.com>
2020-01-29 23:44:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class App < Sinatra::Base
       erb :super_hero
     end
     
+  #   post '/teams' do
+  #     @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+  #     members = params[:team][:members]
+  #     @super_heroes = members.collect do |member_params|
+  #       SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+  #     end
+  #     erb :team
+  #   end
+    
+  # end
+  
     post "/teams" do 
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
@@ -20,8 +31,7 @@ class App < Sinatra::Base
         @hero_power << hero[:power]
         @hero_bio << hero[:bio]
       end
-
       erb :team
-      
     end
-end
+  end
+    
